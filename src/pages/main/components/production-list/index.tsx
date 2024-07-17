f2d9c3c53fb4d6546b3c8351356557e881822734
menyaboo <tosha.kolmay@gmail.com>
2024-07-17 17:59:45 +0300

    change ProductionList component

--- src/pages/main/components/production-list/index.tsx
+++ src/pages/main/components/production-list/index.tsx
@@ -9,10 +9,10 @@ const ProductionList: FC = () => (
     <div className="grid grid-cols-auto-fit-250 text-white gap-6 pt-6">
       {
         coalList.map(({title, image, description}, i) =>
-          <div key={i} className="flex text-center flex-col justify-center items-center bg-gray-700 p-4 rounded-md hover:scale-105 transition cursor-pointer">
+          <div key={i} className="flex text-center flex-col gap-2 justify-center items-center bg-gray-700 p-4 rounded-md hover:scale-105 transition cursor-pointer">
             <h5>{title}</h5>
             <img className="object-contain w-full max-h-52" src={image} alt="coal"/>
-            {description.map((desc, i) => <p key={i}>{desc}</p>)}
+            <ul>{description.map((desc, i) => <li key={i}>{desc}</li>)}</ul>
           </div>
         )
       }
